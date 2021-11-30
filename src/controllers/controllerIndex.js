aa504d3d22c0d46d8d7889c1d04a17c8b9ded455
retamozok <matiasagustin.1823@gmail.com>
2021-11-30 00:37:49 -0300

    Estructura

---
+++ src/controllers/controllerIndex.js
@@ -0,0 +1,10 @@
+let publi = [
+
+]
+
+export const viewpubli = (req,res)=>{
+    res.status(200).render('publicacion',{carts:carts})
+    
+  }
+  
+ 
---
+++ src/routes/routesIndex.js
@@ -0,0 +1,8 @@
+import * as controllerIndex from '../controllers/controllerIndex'
+
+
+const routesCarts = (app) => {
+    app.get('/instagram',controller.controllerIndex)
+    
+export default routesIndex
+}
